18bc9ea3b19c3ee3464859a1dd28988bf25a9fa1
Anisa Priski Liana <anisaprisky123@gmail.com>
2016-11-01 07:03:17 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -29,5 +29,23 @@ public class MainActivity extends AppCompatActivity {
                             startActivity(intent);
                     }
                 });
+
+        findViewById(R.id.imageViewSMS)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        composesSmsMessage("Pesan dari SMK Telkom Malang");
+                    }
+
+                    private void composesSmsMessage(String message)
+                    {
+                        Intent intent = new Intent(Intent.ACTION_SEND);
+                        intent.setType("text/plain");
+                        intent.putExtra("sms_body", message);
+                        if (intent.resolveActivity(getPackageManager()) != null)
+                            startActivity(intent);
+                    }
+                });
     }
 }
